f9a530bb6ddb6e8b2d6faca0378db277bdafccb4
Ye lin aung <hello.yelinaung@gmail.com>
2016-07-19 17:51:27 +0630

    Added New Signed In Activity

---
+++ FirebaseLogin/app/src/main/java/com/yelinaung/firebaselogin/LoggedInActivity.kt
@@ -0,0 +1,12 @@
+package com.yelinaung.firebaselogin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoggedInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged_in)
+    }
+}
